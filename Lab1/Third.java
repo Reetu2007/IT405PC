f8383b0172b18d90843dc7f22f6bc2ee57b62552
Reetu2007 <reetusangisetti@gmail.com>
2026-02-17 12:31:23 +0530

    Add TypeConversion class with type conversion examples
    Demonstrate implicit and explicit type conversion in Java.

--- Lab1/Third.java
+++ Lab1/Third.java
@@ -1,1 +1,38 @@
 
+import java.util.Scanner;
+
+public class TypeConversion {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        int num1 = 10;
+        long num2 = num1;   
+
+        System.out.println("Implicit Type Conversion:");
+        System.out.println("int value: " + num1);
+        System.out.println("long value: " + num2);
+
+        
+        int x = 10;
+        double y = x;        
+        float z = (float) y; 
+
+        long num = 400;
+        int a = (int) num;    
+
+        System.out.println("\nExplicit Type Casting:");
+        System.out.println("double value: " + y);
+        System.out.println("float value: " + z);
+        System.out.println("long value: " + num);
+        System.out.println("int value after casting: " + a);
+
+        sc.close();
+    }
+}
+
+
+
+Program 3
